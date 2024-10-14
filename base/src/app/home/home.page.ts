173bd20ded6093f553c65559e05589be87393679
GlennTatum <glennetatum@gmail.com>
2024-10-14 11:08:20 -0400

    angular material, avatar component

--- base/src/app/home/home.page.ts
+++ base/src/app/home/home.page.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { AvatarComponent } from '../user/avatar/avatar.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, AvatarComponent],
 })
 export class HomePage {
+  
   constructor() {}
 }
